8e069a4d3af10c435cece7ec79465134ecf72ccc
jilen <jilen.zhang@gmail.com>
2023-04-17 19:51:54 +0800

    untabfy

--- cvface/src/main/java/cvface/CVFace.java
+++ cvface/src/main/java/cvface/CVFace.java
@@ -16,28 +16,28 @@ import org.opencv.core.Mat;
 
 public class CVFace {
 
-	/**
-	 * Wrap a buffered image into a Mat.
-	 * Note the returned Mat must not be closed, since its content is managed by jvm
-	 */
-	public static Mat bufferedImageToMat(BufferedImage bi) {
-		byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
-		return new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3, ByteBuffer.wrap(data));
-	}
+  /**
+   * Wrap a buffered image into a Mat.
+   * Note the returned Mat must not be closed, since its content is managed by jvm
+   */
+  public static Mat bufferedImageToMat(BufferedImage bi) {
+    byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+    return new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3, ByteBuffer.wrap(data));
+  }
 
-	public static void loadLib() throws IOException {
-		String libName = Core.NATIVE_LIBRARY_NAME;
-		String fullLibName = System.mapLibraryName(libName);
-		System.out.println("loading lib " + fullLibName);
-		InputStream libInStream = CVFace.class.getClassLoader().getResourceAsStream("opencv/" + fullLibName);
-		Path tmpLib = Paths.get(System.getProperty("java.io.tmpdir"), fullLibName);
-		if (Files.exists(tmpLib)) {
-			Files.delete(tmpLib);
-		}
-		Files.copy(libInStream, tmpLib);
-		Files.setPosixFilePermissions(tmpLib, PosixFilePermissions.fromString("rwxr-xr-x"));
+  public static void loadLib() throws IOException {
+    String libName = Core.NATIVE_LIBRARY_NAME;
+    String fullLibName = System.mapLibraryName(libName);
+    System.out.println("loading lib " + fullLibName);
+    InputStream libInStream = CVFace.class.getClassLoader().getResourceAsStream("opencv/" + fullLibName);
+    Path tmpLib = Paths.get(System.getProperty("java.io.tmpdir"), fullLibName);
+    if (Files.exists(tmpLib)) {
+      Files.delete(tmpLib);
+    }
+    Files.copy(libInStream, tmpLib);
+    Files.setPosixFilePermissions(tmpLib, PosixFilePermissions.fromString("rwxr-xr-x"));
 
-		System.load(tmpLib.toAbsolutePath().toString());
-	}
+    System.load(tmpLib.toAbsolutePath().toString());
+  }
 
 }
